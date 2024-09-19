9bf3a4487d21ca3d2bbc87f08dd6faae9ebe4663
klad <kamlad@users.noreply.github.com>
2024-09-19 04:58:21 +0000

    almost finished useUser

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+
+import React from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { SessionContextProvider } from "@supabase/auth-helpers-react"
+
+import {Database} from "@/types_db";
+
+interface SupabaseProviderProps {
+    children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
+    children
+}) => {
+    const [supabaseCleint] = useState(() =>
+        createClientComponentClient<Database>()
+    );
+
+    return(
+            <SessionContextProvider supabaseCleint={supabaseClient}>
+                {children}
+            </SessionContextProvider>
+    )
+}
+
+export default SupabaseProvider;
